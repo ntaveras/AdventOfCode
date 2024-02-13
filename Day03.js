9f7ea870963d71ae8bb767a0aa1d28f477907ee3
ntaveras <narayanataveras@gmail.com>
2024-02-12 21:18:13 -0500

    Day #3: Part One

--- Day03.js
+++ Day03.js
@@ -1,6 +1,30 @@
 class Day3 {
 
-    partOne(){
-        
+    partOne(input){
+        let xCoordinate = 0;
+        let yCoordinate = 0;
+        const grid = new Map();
+        const elfDirections = input.split('');
+        let counter = 0;
+        elfDirections.forEach(element => {
+            switch(element){
+                case ">" :
+                    xCoordinate++;
+                    break;
+                case "<" :
+                    xCoordinate--;
+                    break
+                case "^" :
+                    yCoordinate++;
+                    break;
+                case "v" :
+                    yCoordinate--;
+            }
+            const coordinate = `${xCoordinate}x${yCoordinate}`;
+            const tmp = (grid.get(coordinate) ?? 0) + 1;
+            grid.set(coordinate, tmp);
+            if(tmp == 1) counter++;
+        });
+        return counter;
     }
 }
